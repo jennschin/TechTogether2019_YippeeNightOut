a068859bca2d1a94c8534742271318e66b9b073a
jennschin <40873250+jennschin@users.noreply.github.com>
2019-03-23 10:27:23 -0400

    Update dish.py

--- dish.py
+++ dish.py
@@ -1,2 +1,18 @@
+'''
+Class representing one dish at one Restaurant. Contains methods to add a review
+and make changes to the dish.
+'''
+
 class dish:
-    
+    __name = ""
+    __Restaurant
+    __list_of_reviews = 0
+
+    def __init__(self, name, Restaurant):
+        self.name = name
+        self.Restaurant = Restaurant
+
+    def add_review(Review):
+        self.list_of_reviews.add(Review)
+        
+        
